02ac331d2c0745f5ffc85dd4829ae8af6804006c
BuildTools <unconfigured@null.spigotmc.org>
2022-02-12 14:34:10 +0200

    fix

--- x86_64/configure.fish
+++ x86_64/configure.fish
@@ -1,2 +1,2 @@
 rm messenger-repo.db messenger-repo.files
-mv mssenger-repo.db.tar.gz messenger-repo.db && mv messenger-repo.files.tar.gz messenger-repo.files
+mv messenger-repo.db.tar.gz messenger-repo.db && mv messenger-repo.files.tar.gz messenger-repo.files
